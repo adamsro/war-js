a12ceb7d3de8321db2e5d0360a0d48e9fd93bcb0
Robert Marshall Adams <r.marshall.adams@gmail.com>
2017-01-13 00:42:14 -0800

    Cards delt. Deck fails test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const PLAYER_A = "A";
 const PLAYER_B = "B";
 
 // The deck is divided evenly among the players, giving each a down stack.
-export class Deck {
+export class CardDeck {
   constructor() {
     const RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
         SUITS = ['s', 'c', 'd', 'h'];
@@ -31,26 +31,44 @@ export class Deck {
 function Card(props) {
   return (<div className="card-spot">
         {props.isBack &&
-          <img class="card" src="/media/{props.rank}-{props.suit}.svg"/>
+        <img class="card" src="/media/{props.rank}-{props.suit}.svg"/>
         }</div>
   );
 }
+
 function PlayerStack(props) {
   return (
       <div>
-        <div className="stack {props.input && active}" onClick={() => props.onClick()}>
+        <div className="stack {props.input && active}" onClick={props.clickHandle}>
           <Card />
         </div>
-        <p className="text-center">Player {props.player}<br /> {props.playerCards} cards remaining</p>
+        <p className="text-center">Player {props.player}<br /> {props.playerCardCount} cards remaining</p>
+      </div>
+  );
+}
+
+function GameControl(props) {
+  return (
+      <div className="war-game-control">
+        <button onClick={() => props.onClick()}>{props.status == IN_PROGRESS ? "restart" : "start"}</button>
       </div>
   );
 }
 
 class WarCardGame extends React.Component {
 
-  newGame() {
+  constructor() {
+    super();
+    this.state = {
+      status: NOT_STARTED,
+      stackA: [],
+      stackB: []
+    }
+  }
+
+  handleStartButton() {
     // New shuffled deck
-    const deck = new Deck();
+    const deck = new CardDeck();
 
     this.setState({
       stackA: deck.getCards(26),
@@ -60,22 +78,37 @@ class WarCardGame extends React.Component {
     });
   }
 
+  handleNextPlayButtons() {
+    this.setState({});
+  }
+
   render() {
     return (
         <div className="war-game">
-          <div className="card-col">
-            <PlayerStack/>
-          </div>
-          <div className="card-col">
-            <Card/>
-            <Card/>
-          </div>
-          <div className="card-col">
-            <Card/>
-            <Card/>
-          </div>
-          <div className="card-col">
-            <PlayerStack/>
+          <GameControl onClick={() => this.handleStartButton()} status={this.state.status}/>
+          <div className="card-layout">
+            <div className="card-col">
+              <PlayerStack
+                  clickHandle={this.handleNextPlayButtons}
+                  player={PLAYER_A}
+                  playerCardCount={this.state.stackA.length}
+              />
+            </div>
+            <div className="card-col">
+              <Card/>
+              <Card/>
+            </div>
+            <div className="card-col">
+              <Card/>
+              <Card/>
+            </div>
+            <div className="card-col">
+              <PlayerStack
+                  clickHandle={this.handleNextPlayButtons}
+                  player={PLAYER_B}
+                  playerCardCount={this.state.stackB.length}
+              />
+            </div>
           </div>
         </div>
     );
--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import CardDeck from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
 it('Deck maintains 52 cards', () => {
-  deck = new Deck();
-  deck.getCards(26);
+  let deck = new CardDeck();
+  expect(deck.getCards(26).length).toBe(26);
+  expect(deck.getCards(26).length).toBe(26);
 });
