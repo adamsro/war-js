a0f321b636ba783deed8268588b42de69f799dbf
Robert Marshall Adams <r.marshall.adams@gmail.com>
2017-01-13 16:53:43 -0800

    Cards showing. no comparison logic

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const PLAYER_B = "B";
 // The deck is divided evenly among the players, giving each a down stack.
 export class CardDeck {
   constructor() {
+    // No jokers
     const RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
         SUITS = ['s', 'c', 'd', 'h'];
     this.deck = [];
@@ -24,33 +25,47 @@ export class CardDeck {
   }
 
   getCards(numCards) {
-    return this.deck.splice(numCards);
+    return this.deck.splice(0, numCards);
+  }
+
+  static compareCards(card1, card2) {
+
   }
 }
 
 function Card(props) {
   return (<div className="card-spot">
-        {props.isBack &&
-        <img class="card" src="/media/{props.rank}-{props.suit}.svg"/>
-        }</div>
+        {props.showBack &&
+        <img className="card" src="/cards/back.svg"/>
+        }
+        {props.face &&
+        <img className="card" src={"/cards/" + props.face.rank + props.face.suit + ".svg"}/>
+        }
+      </div>
   );
 }
 
 function PlayerStack(props) {
-  return (
-      <div>
-        <div className="stack {props.input && active}" onClick={props.clickHandle}>
-          <Card />
+  if (props.clickHandle) {
+    return (<div className="stack active" tabIndex="2" autoFocus="true" onClick={() => props.clickHandle()}>
+          <Card showBack={props.playerCardCount !== 0}/>
+          <p className="text-center">Player {props.player}<br /> {props.playerCardCount} cards remaining</p>
         </div>
-        <p className="text-center">Player {props.player}<br /> {props.playerCardCount} cards remaining</p>
-      </div>
-  );
+    );
+  } else {
+    return (<div className="stack">
+          <Card showBack={props.playerCardCount !== 0}/>
+          <p className="text-center">Player {props.player}<br /> {props.playerCardCount} cards remaining</p>
+        </div>
+    );
+  }
 }
 
 function GameControl(props) {
   return (
       <div className="war-game-control">
-        <button onClick={() => props.onClick()}>{props.status == IN_PROGRESS ? "restart" : "start"}</button>
+        <button onClick={() => props.clickHandle()}
+                tabIndex="1">{props.status === IN_PROGRESS ? "restart" : "start"}</button>
       </div>
   );
 }
@@ -59,52 +74,61 @@ class WarCardGame extends React.Component {
 
   constructor() {
     super();
+    this.deck = new CardDeck();
     this.state = {
       status: NOT_STARTED,
       stackA: [],
-      stackB: []
+      stackB: [],
+      cardA: false,
+      cardB: false,
+      isWar: false,
     }
   }
 
   handleStartButton() {
     // New shuffled deck
-    const deck = new CardDeck();
-
     this.setState({
-      stackA: deck.getCards(26),
-      stackB: deck.getCards(26),
       status: IN_PROGRESS,
-      isNext: PLAYER_A,
+      stackA: this.deck.getCards(26),
+      stackB: this.deck.getCards(26),
+      cardA: false,
+      cardB: false,
+      isWar: false,
     });
   }
 
   handleNextPlayButtons() {
-    this.setState({});
+    const cardA = this.state.stackA.pop();
+    const cardB = this.state.stackB.pop();
+    this.setState({
+      cardA: cardA,
+      cardB: cardB,
+    });
   }
 
   render() {
     return (
         <div className="war-game">
-          <GameControl onClick={() => this.handleStartButton()} status={this.state.status}/>
+          <GameControl clickHandle={() => this.handleStartButton()} status={this.state.status}/>
           <div className="card-layout">
             <div className="card-col">
               <PlayerStack
-                  clickHandle={this.handleNextPlayButtons}
+                  clickHandle={() => this.handleNextPlayButtons()}
                   player={PLAYER_A}
                   playerCardCount={this.state.stackA.length}
               />
             </div>
             <div className="card-col">
-              <Card/>
-              <Card/>
+              <Card face={this.state.cardA} />
+              <Card showBack={this.state.isWar} />
             </div>
             <div className="card-col">
-              <Card/>
-              <Card/>
+              <Card face={this.state.cardB} />
+              <Card showBack={this.state.isWar} />
             </div>
             <div className="card-col">
               <PlayerStack
-                  clickHandle={this.handleNextPlayButtons}
+                  clickHandle={false}
                   player={PLAYER_B}
                   playerCardCount={this.state.stackB.length}
               />
