da96d1d527ec9f8f08bfa5bfa760ea54a1154e3b
Robert Marshall Adams <r.marshall.adams@gmail.com>
2017-01-18 19:38:03 -0800

    Make app.test.js filename lowercase

--- src/app.test.js
+++ src/app.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {App, CardDeck, PlayingCard} from 'App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
